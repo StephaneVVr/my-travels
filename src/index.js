ce839d6f34183b3fe5c62ca88aced3c79e741313
Stéphane Vivier <contact.xsprod@gmail.com>
2019-03-26 18:18:17 +0100

    React two quest commit

--- src/index.js
+++ src/index.js
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Travel from './travel';
 
-const travelOne = {
-    destination: 'Quelque part',
-    country: 'Dans le monde',
-    image: 'https://www.acs-ami.com/fr/blog/wp-content/uploads/2015/07/inspiration-voyage.jpg',
-    distance: 'Loin'
+const travelArray = [
+
+    {
+        destination: 'Quelque part',
+        country: 'Dans le monde',
+        image: 'https://www.acs-ami.com/fr/blog/wp-content/uploads/2015/07/inspiration-voyage.jpg',
+        distance: 'Loin'
+    },
+    {
+        destination: 'Banane',
+        country: 'Courgette',
+        image: 'http://www.unmonde.fr/wp-content/uploads/2014/05/Fotolia_43783967_Subscription_S.jpg',
+        distance: '45 000km'
+    },
+    {
+        destination: 'Banane',
+        country: 'Courgette',
+        image: 'http://www.unmonde.fr/wp-content/uploads/2014/05/Fotolia_43783967_Subscription_S.jpg',
+        distance: '45 000km'
+    },
+    {
+        destination: 'Banane',
+        country: 'Courgette',
+        image: 'http://www.unmonde.fr/wp-content/uploads/2014/05/Fotolia_43783967_Subscription_S.jpg',
+        distance: '45 000km'
+    },
+    {
+        destination: 'Banane',
+        country: 'Courgette',
+        image: 'http://www.unmonde.fr/wp-content/uploads/2014/05/Fotolia_43783967_Subscription_S.jpg',
+        distance: '45 000km'
+    }
+];
+
+class Files extends Component {
+    render() {
+        return ( 
+            travelArray.map((array) => {
+            return <Travel {...array} />
+        }))
+        
+    }
 };
 
-const travelTwo = {
-    destination: 'Banane',
-    country: 'Courgette',
-    image: 'http://www.unmonde.fr/wp-content/uploads/2014/05/Fotolia_43783967_Subscription_S.jpg',
-    distance: '45 000km'
-};
+ReactDOM.render(<Files />, document.getElementById('root'));
 
-const App = () => <div>
-    <Travel {...travelOne} /> 
-    <Travel {...travelTwo} />
-</div>
-    
-ReactDOM.render(<App />, document.getElementById('root'));
-    
